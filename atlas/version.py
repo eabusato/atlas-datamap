d8419c54be2f5f28a660bf80e5e7ac9af2c8769b
Erick Busato <eabusato@gmail.com>
2026-04-02 22:45:58 -0300

    build and publish yml debugs

--- atlas/version.py
+++ atlas/version.py
@@ -1,6 +1,6 @@
 """Version constants for the Atlas Datamap package."""
 
-ATLAS_VERSION = "1.0.0"
+ATLAS_VERSION = "1.0.1"
 ATLAS_MIN_PYTHON = (3, 11)
 ATLAS_C_SIGILO_VERSION = "0.40.0"
 ATLAS_BRIDGE_PROTOCOL_VERSION = 1
